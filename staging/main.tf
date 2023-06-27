631c16743e2d0094005ca1451d9cb97bc40a831a
tomoki.maeda <you@example.com>
2023-06-27 18:43:34 +0900

    comment out

--- staging/main.tf
+++ staging/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
+  #required_providers {
+  #  azurerm = {
+  #    source  = "hashicorp/azurerm"
+  #    version = "~> 3.0.2"
+  #  }
+  #}
   cloud {
     organization = "tmaeda-hashicorp"
     hostname     = "app.terraform.io"
@@ -18,10 +18,10 @@ terraform {
 # general credentials setting
 provider "azurerm" {
   features {}
-#  subscription_id = var.subscription_id
-#  tenant_id       = var.tenant_id
-#  client_id       = var.client_id
-#  client_secret   = var.client_secret
+  #  subscription_id = var.subscription_id
+  #  tenant_id       = var.tenant_id
+  #  client_id       = var.client_id
+  #  client_secret   = var.client_secret
 }
 
 #module "resource_group" {

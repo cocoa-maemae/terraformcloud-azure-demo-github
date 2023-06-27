8e4448cf0770ce5666ce57f3397c33fc196ccd57
tomoki.maeda <you@example.com>
2023-06-27 19:59:21 +0900

    fix

--- staging/main.tf
+++ staging/main.tf
@@ -31,12 +31,3 @@ module "resource_group" {
 module "storage_account" {
   source = "./../module/storage_account"
 }
-
-#locals {
-#  env = substr("${terraform.workspace}", 26, -1)
-#}
-
-#resource "azurerm_resource_group" "example" {
-#  name     = "example-resource-group-${local.env}"
-#  location = "japaneast"
-#}
